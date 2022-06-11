cbf78a80b46cc1aff2be4fd129516cefe1d11bf7
Kim min jun <20181796@edu.hanbat.ac.kr>
2022-06-11 14:48:16 +0900

    design

--- app/src/main/java/com/example/project/CheckCurrentUser.java
+++ app/src/main/java/com/example/project/CheckCurrentUser.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -13,6 +15,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,8 +31,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.Format;
 import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -30,16 +44,74 @@ public class CheckCurrentUser extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference currDatabaseRef;
     String currUser;
-    int userNum = 64;
+    int userNum = 0;
     ProgressBar progressBar;
     ImageButton prev, mypage;
     TextView tv;
-
+    private XYPlot plot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_check_current_user);
+
+        /**
+         * Plot
+         * */
+        plot = (XYPlot) findViewById(R.id.plot);
+        plot.setRangeBoundaries(0, 450, BoundaryMode.FIXED);
+        plot.getLegend().setVisible(false);
+
+
+        // create a couple arrays of y-values to plot:
+        final String[] domainLabels = {"", "11:00", "", "", "12:00", "",
+                "", "13:00", "", ""};
+
+        Number[] series1Numbers = {102, 144, 321, 343, 287, 362, 244, userNum};
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        LineAndPointFormatter series1Format =
+                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
+
+        LineAndPointFormatter series2Format =
+                new LineAndPointFormatter(this, R.xml.line_plot_formatter_labels_2);
+
+        // add an "dash" effect to the series2 line:
+        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
+
+                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                PixelUtils.dpToPix(0),
+                PixelUtils.dpToPix(0)}, 0));
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        series2Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+
+        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = Math.round(((Number) obj).floatValue());
+                return toAppendTo.append(domainLabels[i]);
+            }
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
 
         /********************************************/
         /*
@@ -53,8 +125,7 @@ public class CheckCurrentUser extends AppCompatActivity {
         /*******************************************/
 
         progressBar = findViewById(R.id.progress);
-        progressBar.setMax(100);
-        progressBar.setProgress(userNum);
+        progressBar.setMax(10);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         currDatabaseRef = FirebaseDatabase.getInstance().getReference("project");
@@ -79,7 +150,7 @@ public class CheckCurrentUser extends AppCompatActivity {
             }
         });
         /**
-         *  project/CurrentUser/ 의 String을 가져와서
+         *  Firebase 의 project/CurrentUser/ 의 String을 가져와서
          *  인원 처리
          *
          * */
@@ -127,7 +198,9 @@ public class CheckCurrentUser extends AppCompatActivity {
                                     e.printStackTrace();
                                 }
                             }
-                            tv.setText("현재 식당 이용자 수\n\n"+userNum + "명");
+                            tv.setText("현재 식당 이용자 수\n"+userNum + "명");
+                            progressBar.setProgress(userNum);
+
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
